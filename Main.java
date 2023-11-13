83cb216adc20e5a99f98a853a121ab008d6a02b8
10017708 <92541105+10017708@users.noreply.github.com>
2023-11-13 12:33:15 -0500

    Added Timer
    Added Timer

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -11,38 +12,44 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class Main {
     private static final int NUMBER_OF_THREADS = 100;
+
     public static void main(String[] args) {
+        Instant instantStart = Instant.now();
+
         ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
         AtomicInteger count = new AtomicInteger(0);
         ArrayList<Future<Integer>> allFutures = new ArrayList<Future<Integer>>();
 
-        for(int i = 0; i < NUMBER_OF_THREADS; i++){
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
             Callable<Integer> callable = new Callable<Integer>() {
-				@Override
-				public Integer call() throws Exception {
+                @Override
+                public Integer call() throws Exception {
                     int count = 0;
-                    for(int j = 0; j < 1000000; j++){
+                    for (int j = 0; j < 1000000; j++) {
                         count++;
                     }
                     return count;
-				}
+                }
             };
 
             allFutures.add(executorService.submit(callable));
         }
 
-        for(int i = 0; i < allFutures.size(); i++){
+        for (int i = 0; i < allFutures.size(); i++) {
             Future<Integer> future = allFutures.get(i);
             try {
-				count.addAndGet(future.get());
-			} catch (InterruptedException | ExecutionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+                count.addAndGet(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
 
-        System.out.println(count);
+        Instant instantEnd = Instant.now();
+        long duration = instantEnd.toEpochMilli() - instantStart.toEpochMilli();
 
+        System.out.println("Final count value: " + count.get());
+        System.out.println("Duration: " + duration + " milliseconds");
     }
 }
