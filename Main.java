21303f353ff879898c16b38db8930d281f6c2552
10017708 <92541105+10017708@users.noreply.github.com>
2023-11-13 12:30:14 -0500

    Main Commit
    Added code to count to 100,000,000

---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Main
+ */
+public class Main {
+    private static final int NUMBER_OF_THREADS = 100;
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+        AtomicInteger count = new AtomicInteger(0);
+        ArrayList<Future<Integer>> allFutures = new ArrayList<Future<Integer>>();
+
+        for(int i = 0; i < NUMBER_OF_THREADS; i++){
+            Callable<Integer> callable = new Callable<Integer>() {
+				@Override
+				public Integer call() throws Exception {
+                    int count = 0;
+                    for(int j = 0; j < 1000000; j++){
+                        count++;
+                    }
+                    return count;
+				}
+            };
+
+            allFutures.add(executorService.submit(callable));
+        }
+
+        for(int i = 0; i < allFutures.size(); i++){
+            Future<Integer> future = allFutures.get(i);
+            try {
+				count.addAndGet(future.get());
+			} catch (InterruptedException | ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+
+        System.out.println(count);
+
+    }
+}
